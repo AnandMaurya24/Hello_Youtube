be6add8e40ed93a19f77049d53db586e25b8697f
anandmaurya24 <anandmaurya294@gmail.com>
2024-01-28 19:20:18 +0530

    added form for live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -23,13 +23,38 @@ const LiveChat = () => {
   }, []);
 
   return (
-    <div className="ml-2 p-2 w-full h-[600px] border border-black bg-slate-100 rounded-lg flex overflow-y-scroll flex-col-reverse">
-      <div>
-        {chatMessages.map((c, i) => (
-          <ChatMessages key={i} name={c.name} message={c.message} />
-        ))}
+    <>
+      <div className="ml-2 p-2 w-full h-[600px] border border-black bg-slate-100 rounded-lg flex overflow-y-scroll flex-col-reverse">
+        <div>
+          {chatMessages.map((c, i) => (
+            <ChatMessages key={i} name={c.name} message={c.message} />
+          ))}
+        </div>
       </div>
-    </div>
+      <form
+        className="w-full p-2 m-2 border border-black flex"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Anand Maurya",
+              message: liveMessage,
+            })
+          );
+          setLiveMesasge("");
+        }}
+      >
+        <input
+          className="px-2 w-90 border border-black"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMesasge(e.target.value);
+          }}
+        />
+        <button className="px-2 mx-2 bg-green-100">Send</button>
+      </form>
+    </>
   );
 };
 
